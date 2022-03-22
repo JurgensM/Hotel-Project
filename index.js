34a50a38a0d753f1ad5e8254fff78a8726cfe63f
JurgensM <jurgensmarpali2@icloud.com>
2022-03-22 19:25:54 +0100

    Co-authored-by: Edoardo  <Edo404@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,5 @@
+var mapCanvas = document.getElementById("map");
+var mapOptions = {
+    center: new google.maps.LatLng(3.202778, 	73.220680), zoom: 10
+}
+var map = new google.maps.Map(mapCanvas, mapOptions);
